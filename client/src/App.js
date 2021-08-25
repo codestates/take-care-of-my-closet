fd7502dbe8bf66900e6e10ffad86a5ce9731a3e9
Achates09 <srw0901@gmail.com>
2021-08-25 19:07:08 +0900

    add: buildspec.yml

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+// import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
