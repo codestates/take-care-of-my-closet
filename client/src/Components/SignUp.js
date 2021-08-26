e1298fe793f1e0536b64e4d774b53ebad5f3bc40
james940522 <James940522@gmail.com>
2021-08-26 10:15:43 +0900

    feat: LoginState,SPA

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react"
+import axios from "axios"
 // import logo from '';
 
 function SignUp() {
-  const [idValue, setIdValue] = useState('');
-  const [pwValue, setPwValue] = useState('');
+  const [idValue, setIdValue] = useState("")
+  const [pwValue, setPwValue] = useState("")
 
   const inputIdHandler = (e) => {
-    setIdValue(e.target.value);
+    setIdValue(e.target.value)
     console.log(idValue)
   }
 
-  const isDuplicated = (id) => { // 중복확인 api 요청 함수
+  const isDuplicated = (id) => {
+    // 중복확인 api 요청 함수
     // axios.post('', {
-
     // })
     // .then(result => {
     //   console.log(result)
@@ -30,22 +30,29 @@ function SignUp() {
     <div>
       <h1>img</h1>
       <h2>회원가입</h2>
-        <form>
-          <fieldset>
-            아이디 : <input
-            type='text'
+      <form>
+        <fieldset>
+          아이디 :{" "}
+          <input
+            type="text"
             onChange={(e) => inputIdHandler(e)}
             value={idValue}
-            placeholder='아이디를 입력하세요'
-            />
-            <button onClick={isDuplicated}>중복확인</button><br/>
-            비밀번호 : <input type='password' placeholder='비밀번호를 입력하세요'/><br/>
-            비밀번호 확인 : <input type='password' placeholder='비밀번호 확인'/><br/>
-            프로필 사진 첨부 : <input type='file'/><br/>
-            닉네임 : <input type='text' placeholder='닉네임을 입력하세요'/><br/>
-            <button>가입하기</button>
-          </fieldset>
-        </form>
+            placeholder="아이디를 입력하세요"
+          />
+          <button onClick={isDuplicated}>중복확인</button>
+          <br />
+          비밀번호 :{" "}
+          <input type="password" placeholder="비밀번호를 입력하세요" />
+          <br />
+          비밀번호 확인 : <input type="password" placeholder="비밀번호 확인" />
+          <br />
+          프로필 사진 첨부 : <input type="file" />
+          <br />
+          닉네임 : <input type="text" placeholder="닉네임을 입력하세요" />
+          <br />
+          <button>가입하기</button>
+        </fieldset>
+      </form>
     </div>
   )
 }
