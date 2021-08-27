338e423996b1fa25868e67ab2f4960b763d5ee30
Achates09 <srw0901@gmail.com>
2021-08-27 11:00:39 +0900

    chore: App.js 에러 해결

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import "./App.css"
-import React, { useState } from "react"
-import { Switch, Route, useLocation } from "react-router-dom"
-import Nav from "./Components/Nav"
-import Footer from "./Components/Footer"
+import "./App.css";
+import React, { useState } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
 
-import Login from "./Components/Login"
-import Main from "./Components/Main"
-import MyContents from "./Components/MyContents"
-import MyPage from "./Components/MyPage"
-import SignUp from "./Components/SignUp"
-import ContentModiCreate from "./Components/ContentsModiCreate"
+import Login from "./Components/Login";
+import Main from "./Components/Main";
+import MyContents from "./Components/MyContents";
+import MyPage from "./Components/MyPage";
+import SignUp from "./Components/SignUp";
+import ContentModiCreate from "./Components/ContentsModiCreate";
 import Content from "./Components/Content";
 
 function App() {
@@ -46,50 +46,48 @@ function App() {
   };
 
   const ChangeLoginState = (boolean) => {
+    setIsLogin(boolean);
+  };
 
-    setIsLogin(boolean)
-  }
+  let loca = useLocation();
 
-  let loca = useLocation()
-
-  console.log("로케이션 제발", loca)
+  console.log("로케이션 제발", loca);
 
   return (
     <React.Fragment>
-        {/* <div> */}
-        {loca.pathname === "/login" || loca.pathname === "/signup" ? (
-            <></>
-          ) : (
-            <Nav isLogin={isLogin} ChangeLoginState={ChangeLoginState} />
-            <section>
-              <Switch>
-                <Route exact path="/">
-                  <Main />
-                </Route>
-                <Route path="/login">
-                  <Login loginHandler={loginHandler} />
-                </Route>
-                <Route path="/signup">
-                  <SignUp />
-                </Route>
-                <Route path="/mycontents">
-                  <MyContents isLogin={isLogin} />
-                </Route>
-                <Route path="/mypage">
-                  <MyPage isLogin={isLogin} />
-                </Route>
-                <Route path="/content">
-                  <Content
-                    isLogin={isLogin}
-                    userInfo={userInfo}
-                    accessToken={accessToken}
-                  />
-                </Route>
-              </Switch>
-            </section>
-          </main>
-        </div>
-        <Footer />
+      {/* <div> */}
+      {loca.pathname === "/login" || loca.pathname === "/signup" ? (
+        <></>
+      ) : (
+        <Nav isLogin={isLogin} ChangeLoginState={ChangeLoginState} />
+      )}
+      <section>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/login">
+            <Login loginHandler={loginHandler} />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/mycontents">
+            <MyContents isLogin={isLogin} />
+          </Route>
+          <Route path="/mypage">
+            <MyPage isLogin={isLogin} />
+          </Route>
+          <Route path="/content">
+            <Content
+              isLogin={isLogin}
+              userInfo={userInfo}
+              accessToken={accessToken}
+            />
+          </Route>
+        </Switch>
+      </section>
+      <Footer />
     </React.Fragment>
   );
 }
