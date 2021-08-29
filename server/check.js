90aeb7fbf8ec1a8aa34c68254771bf3f0de33657
james940522 <James940522@gmail.com>
2021-08-30 05:39:23 +0900

    check

---
+++ server/check.js
@@ -0,0 +1,10 @@
+console.log({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  name: process.env.DATABASE_NAME,
+  acc: process.env.ACCESS_SECRET,
+  re: process.env.REFRESH_SECRET,
+  ID: process.env.ID,
+  PASS: process.env.PASS,
+})
