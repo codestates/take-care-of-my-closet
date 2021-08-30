62df9dccc947157c769636331b1923f98b6ac8d6
james940522 <James940522@gmail.com>
2021-08-30 22:31:39 +0900

    chore test port4

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/take-care-of-my-closet/server
+
 
 export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
@@ -10,5 +10,7 @@ export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names AC
 export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
 export ID=$(aws ssm get-parameters --region ap-northeast-2 --names ID --query Parameters[0].Value | sed 's/"//g')
 export PASS=$(aws ssm get-parameters --region ap-northeast-2 --names PASS --query Parameters[0].Value | sed 's/"//g')
+
+cd /home/ubuntu/take-care-of-my-closet/server
 
 authbind --deep pm2 start index.js
