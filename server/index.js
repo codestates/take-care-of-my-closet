08afa593882c0e753d8e50c9b227ebb1f6c06dae
james940522 <James940522@gmail.com>
2021-09-01 15:04:13 +0900

    chore: error

--- server/index.js
+++ server/index.js
@@ -26,6 +26,12 @@ app.use(
 //   .catch(console.error)
 
 app.use(cookieParser())
+
+//get Server
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome, closet Server!")
+})
+
 //user
 app.post("/login", controllers.login)
 app.get("/accessTokenrequest", controllers.accessTokenRequest)
@@ -52,8 +58,10 @@ app.post("/deletecomment", controllers.deletepost)
 app.post("/createFakeData", controllers.createFakeData)
 app.post("/upload", upload.single("closet"), controllers.upload)
 
-const HTTPS_PORT = 80
+const HTTPS_PORT = 4000
 let server
-server = app.listen(HTTPS_PORT)
+server = app.listen(HTTPS_PORT, () => {
+  console.log("server 실행")
+})
 
 module.exports = server
