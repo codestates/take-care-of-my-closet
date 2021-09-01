f26192967a52e844eda5c1fc4653e8ff661ad0ac
james940522 <James940522@gmail.com>
2021-09-01 14:54:35 +0900

    chore index.js

--- server/models/index.js
+++ server/models/index.js
@@ -40,11 +40,7 @@ Object.keys(db).forEach((modelName) => {
   }
 })
 
-<<<<<<< HEAD
 const { user, post, comment, refreshtoken } = sequelize.models
-=======
-const { user, post, comment ,refreshtoken} = sequelize.models
->>>>>>> 7aa404179257ae6bc00cc6f33c239d68c3f85290
 
 post.belongsTo(user)
 user.hasMany(post)
@@ -77,9 +73,7 @@ post.belongsToMany(user, {
   foreignKey: "postId",
 })
 
-user.hasOne(refreshtoken);
-
-
+user.hasOne(refreshtoken)
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
