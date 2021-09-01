9a531a61f47c57e9446af9eaba106415002be8ad
Kangjoohyun <noonssub1004@gmail.com>
2021-09-01 16:04:28 +0900

    test

--- server/seeders/20210826150420-user.js
+++ server/seeders/20210826150420-user.js
@@ -16,7 +16,7 @@ module.exports = {
       {
         login_id: "dailyLook123",
         password: "test1",
-        user_image: "image1",
+        user_image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         nickname: "데일리룩",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -24,7 +24,7 @@ module.exports = {
       {
         login_id: "sadFlog",
         password: "test2",
-        user_image: "image2",
+        user_image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         nickname: "슬픈개구릐",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -32,7 +32,7 @@ module.exports = {
       {
         login_id: "hyerx",
         password: "test3",
-        user_image: "image3",
+        user_image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         nickname: "라니",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -40,7 +40,7 @@ module.exports = {
       {
         login_id: "james7507",
         password: "test4",
-        user_image: "image4",
+        user_image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         nickname: "산다라밥",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -48,7 +48,7 @@ module.exports = {
       {
         login_id: "devil112",
         password: "test5",
-        user_image: "image4",
+        user_image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         nickname: "아재사랑",
         createdAt: new Date(),
         updatedAt: new Date(),
--- server/seeders/20210826160621-post.js
+++ server/seeders/20210826160621-post.js
@@ -14,7 +14,7 @@ module.exports = {
     return queryInterface.bulkInsert("posts", [
       {
         title: "스트릿 코디입니다.",
-        image: "image1",
+        image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         contents: "길 한복판에서 착샷 찍는건 민망.. 후다닥 찍음",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -22,7 +22,7 @@ module.exports = {
       },
       {
         title: "소개팅 코디",
-        image: "image2",
+        image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         contents: "예전에 놀러갈때 룩.. 소개팅 자리에도 괜찮을까요?",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         title: "작년 겨울 코디 올해도 이륙 가능할까요?",
-        image: "image3",
+        image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         contents: "가을 준비 다해놔서 이제 겨울을 준비한다.",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -38,7 +38,7 @@ module.exports = {
       },
       {
         title: "게이 같나요?",
-        image: "image4",
+        image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         contents: "어제 전시회 다녀온 착.. 여사친이 게이같다고 놀리네요..",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -46,7 +46,7 @@ module.exports = {
       },
       {
         title: "오늘의 착샷",
-        image: "image4",
+        image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         contents: "날씨 선선해지니 가을 코디",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -54,7 +54,7 @@ module.exports = {
       },
       {
         title: "샌들 샀습니다~",
-        image: "image4",
+        image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         contents: "반바지에~ 마눌님이 사준 양말~ 가을에도 괜찮죠~?^^",
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -62,7 +62,7 @@ module.exports = {
       },
       {
         title: "이 조합 괜찮나요??",
-        image: "image4",
+        image: "https://take-closet-bucket.s3.ap-northeast-2.amazonaws.com/1630478307674.jpg",
         contents: "고수님들 평가좀 부탁 드립니다ㅠㅠ",
         createdAt: new Date(),
         updatedAt: new Date(),
