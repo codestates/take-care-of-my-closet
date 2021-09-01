03c8215b02da5e3fbb36045d6715ec2a1956a633
Achates09 <srw0901@gmail.com>
2021-09-01 19:35:46 +0900

    fix: 새글 작성시 메인 페이지 이동후 새 게시글 랜더링

--- server/controllers/comment/DeleteComment.js
+++ server/controllers/comment/DeleteComment.js
@@ -1,13 +1,13 @@
-const { comment } = require("../../models")
+const { comment } = require("../../models");
 
 module.exports = async (req, res) => {
-  console.log(req.body)
+  console.log("댓글삭제 요청", req.body);
 
   //{postId: 현재 게시글 아이디(pk), userId, 현재 댓글 작성한 유저 아이디(pk), comments: 댓글 내용}
 
   await comment.destroy({
     where: { id: req.body.id },
-  })
+  });
 
-  res.status(200).json({ message: "delete!" })
-}
+  res.status(200).json({ message: "delete!" });
+};
