8127e5e7b659344f6edee84b658a037c13f91843
james940522 <James940522@gmail.com>
2021-09-02 11:17:55 +0900

    destroy storybook

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
-const cors = require("cors");
-const db = require("./models/index");
-const express = require("express");
-const app = express();
-const controllers = require("./controllers");
-const cookieParser = require("cookie-parser");
-const upload = require("./modules/multer");
+require("dotenv").config()
+const cors = require("cors")
+const db = require("./models/index")
+const express = require("express")
+const app = express()
+const controllers = require("./controllers")
+const cookieParser = require("cookie-parser")
+const upload = require("./modules/multer")
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use(
   cors({
@@ -16,55 +16,53 @@ app.use(
     credentials: true,
     methods: ["GET", "OPTIONS", "POST", "PUT"],
   })
-);
+)
 
 db.sequelize
   .sync()
   .then(() => {
-    console.log("db 연결 ");
+    console.log("db 연결 ")
   })
-  .catch(console.error);
+  .catch(console.error)
 
-app.use(cookieParser());
+app.use(cookieParser())
 
 //get Server
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome, closet Server!");
-});
+  res.status(200).send("Welcome, closet Server!")
+})
 
 //user
-app.post("/login", controllers.login);
-app.get("/accessTokenrequest", controllers.accessTokenRequest);
-app.get("/refreshTokenrequest", controllers.refreshTokenRequest);
-app.post("/signup", controllers.signup);
-app.get("/logout", controllers.logout);
-app.post("/passwordCheck", controllers.passwordCheck);
-app.post("/duplicate", controllers.duplicate);
-app.put("/modifyuserinfo", controllers.modifyuserinfo);
+app.post("/login", controllers.login)
+app.get("/accessTokenrequest", controllers.accessTokenRequest)
+app.get("/refreshTokenrequest", controllers.refreshTokenRequest)
+app.post("/signup", controllers.signup)
+app.get("/logout", controllers.logout)
+app.post("/passwordCheck", controllers.passwordCheck)
+app.post("/duplicate", controllers.duplicate)
+app.put("/modifyuserinfo", controllers.modifyuserinfo)
 
 //post
-app.post("/getposts", controllers.getPosts);
-app.post("/getContents", controllers.getContents);
-app.put("/modifymypost", controllers.modifymypost);
-app.post("/likeunlike", controllers.likeunlike);
-app.post("/deletepost", controllers.deletepost);
-app.post("/createpost", controllers.createpost);
+app.post("/getposts", controllers.getPosts)
+app.post("/getContents", controllers.getContents)
+app.put("/modifymypost", controllers.modifymypost)
+app.post("/likeunlike", controllers.likeunlike)
+app.post("/deletepost", controllers.deletepost)
+app.post("/createpost", controllers.createpost)
 
 //comment
-app.post("/createComment", controllers.createComment);
-app.post("/deletecomment", controllers.deletecomment);
+app.post("/createComment", controllers.createComment)
+app.post("/deletecomment", controllers.deletecomment)
 
 //etc
-app.post("/createFakeData", controllers.createFakeData);
-app.post("/upload", upload.single("closet"), controllers.upload);
+app.post("/createFakeData", controllers.createFakeData)
+app.post("/upload", upload.single("closet"), controllers.upload)
 
 const HTTPS_PORT = 4000
 let server
 
-
-
 server = app.listen(HTTPS_PORT, () => {
-  console.log("server 실행");
-});
+  console.log("server 실행")
+})
 
-module.exports = server;
+module.exports = server
