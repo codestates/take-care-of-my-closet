00985b5e948591f69cbf6dc6d6de050a09cf3132
james940522 <James940522@gmail.com>
2021-09-02 11:23:33 +0900

    test deploy

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ app.post("/deletecomment", controllers.deletecomment)
 app.post("/createFakeData", controllers.createFakeData)
 app.post("/upload", upload.single("closet"), controllers.upload)
 
-const HTTPS_PORT = 4000
+const HTTPS_PORT = 80
 let server
 
 server = app.listen(HTTPS_PORT, () => {
