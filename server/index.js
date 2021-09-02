a914269d07a77c742e66c68f8fb6ef572d53c614
james940522 <James940522@gmail.com>
2021-09-02 11:28:57 +0900

    reset index.js

--- server/index.js
+++ server/index.js
@@ -18,12 +18,12 @@ app.use(
   })
 )
 
-db.sequelize
-  .sync()
-  .then(() => {
-    console.log("db 연결 ")
-  })
-  .catch(console.error)
+// db.sequelize
+//   .sync()
+//   .then(() => {
+//     console.log("db 연결 ")
+//   })
+//   .catch(console.error)
 
 app.use(cookieParser())
 
@@ -58,7 +58,7 @@ app.post("/deletecomment", controllers.deletecomment)
 app.post("/createFakeData", controllers.createFakeData)
 app.post("/upload", upload.single("closet"), controllers.upload)
 
-const HTTPS_PORT = 80
+const HTTPS_PORT = 4000
 let server
 
 server = app.listen(HTTPS_PORT, () => {
